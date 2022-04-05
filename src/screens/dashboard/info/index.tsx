ca096e857f348e669136a9f02c5397e9f3022dd4
javier ho <tech@craftome.com>
2022-04-05 23:11:48 +0800

    using types

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -3,8 +3,7 @@ import { Text, View } from "react-native";
 import styles from "./styles";
 
 const Info: React.FC = () => {
-const a: string = "a";
-const num: number = "num";
+
   return (
     <View style={styles.container}>
       <Text>
@@ -15,3 +14,15 @@ const num: number = "num";
 };
 
 export default Info;
+
+// TypeScript's type system only helps you during development
+//(ie before the code gets compiled)
+function add(n1,n2) {
+  return n1 + n2;
+}
+
+const number1 = '5';
+const number2  = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
