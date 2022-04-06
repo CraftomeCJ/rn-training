a9ebb970c6a42ce3fc0070631e3b14ac2d7c9407
javier ho <tech@craftome.com>
2022-04-06 12:23:57 +0800

    Important: Type Casing

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -26,14 +26,22 @@ function add(n1: number, n2: number) {
   //The key difference between JS & TS ==>
   // JS uses "dynamic types" (resolved at runtime);
   // TS uses "static types" (set during development).
+
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error("Incorrect input!");
   // }
   return n1 + n2;
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2  = 2.8;
 
 const result = add(number1, number2);
 console.log(result);
+
+// Important: Type Casing
+/* In TypeScript, you work with types like "string" or number
+all the times.
+==> IMPORTANT: It is lowercase "string" and "number",
+NOT "String" and "Number" etc.. <==
+The core primitive types in TypeScript are all lowercase */
