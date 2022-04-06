e1df1d94b144744945c5185e07c5cfcbe9820700
javier ho <tech@craftome.com>
2022-04-06 10:42:12 +0800

    experience how typescript work

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { Text, View } from "react-native";
-import styles from "./styles";
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
 
-const Info: React.FC = () => {
-
-  return (
+const Info: React.FC = () =>
+  // realtime reporting TS error
+  const a: string = "a";
+  const num: number = "num";
+  (
     <View style={styles.container}>
       <Text>
         Info Page: You don't need to login to view this page
       </Text>
     </View>
   );
-};
-
 export default Info;
 
 // TypeScript's type system only helps you during development
-//(ie before the code gets compiled)
+// (ie before the code gets compiled)
 function add(n1,n2) {
   return n1 + n2;
 }
