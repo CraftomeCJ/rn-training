5710970b39c53e8ddb4162c9d7a84675cb339afa
javier ho <tech@craftome.com>
2022-04-06 10:58:12 +0800

    TypeScript Types vs JavaScript Types

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -4,8 +4,8 @@ import styles from './styles';
 
 const Info: React.FC = () =>
   // realtime reporting TS error
-  const a: string = "a";
-  const num: number = "num";
+  // const a: string = "a";
+  // const num: number = "num";
   (
     <View style={styles.container}>
       <Text>
@@ -17,7 +17,18 @@ export default Info;
 
 // TypeScript's type system only helps you during development
 // (ie before the code gets compiled)
-function add(n1,n2) {
+function add(n1: number, n2: number) {
+  //in vanilla javascript
+  //a non TS way of ensuring we can't call the function
+  //it is better to fail at runtime it is better to fail here then showing incorrect output than before
+  //a way to check the input in JS not need TS to do it
+  //downside ==> sometime only can validate certain input in runtime, we check sometime can be avoid during development with TS
+  //The key difference between JS & TS ==>
+  // JS uses "dynamic types" (resolved at runtime);
+  // TS uses "static types" (set during development).
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error("Incorrect input!");
+  // }
   return n1 + n2;
 }
 
