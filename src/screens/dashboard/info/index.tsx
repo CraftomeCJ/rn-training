83f5a2a5c7cd77cf861aa25936bc97f07cc19093
javier ho <tech@craftome.com>
2022-04-08 16:42:12 +0800

    Function - Other Types to Know About - void, object, unknown, never

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -754,3 +754,79 @@ interface CallOrConstruct {
   new (s: string): Date;
   (n?: number): number;
 }
+
+//learn Other Types to Know About
+//There are some additional types you'll want to recognize that appear often when working with function types.
+//Like all types, you can use them everywhere, but these are especially relevant in the context of functions.
+
+//note void
+
+//void represents the return value of functions which don't return a value.
+//It's the inferred type any time a function doesn't have any return statements,
+//or doesn't return any explicit value from those return statements:
+//example The inferred return type is void
+function noop() {
+  return;
+}
+//In JavaScript, a function that doesn't return any value will implicitly return the value undefined.
+//However, void and undefined are not the same thing in TypeScript.
+//There are further details at the end of this chapter.
+
+//important void is not the same as undefined.
+
+//note object
+
+//The special type object refers to any value that isn't a primitive (string, number, bigint, boolean, symbol, null, or undefined).
+//This is different from the empty object type { }, and also different from the global type Object.
+//It’s very likely you will never use Object.
+
+//important object is not Object. Always use object!
+
+//note that in JavaScript, function values are objects: They have properties, have Object.prototype in their prototype chain, are instanceof Object, you can call Object.keys on them, and so on.
+//For this reason, function types are considered to be objects in TypeScript.
+
+//note unknown
+
+//The unknown type represents any value.
+//This is similar to the any type, but is safer because it’s not legal to do anything with an unknown value:
+function f1(a: any) {
+  a.b(); // OK
+}
+/*
+function f2(a: unknown) {
+  a.b();            // Error!
+//Object is of type 'unknown'.
+}
+*/
+
+//This is useful when describing function types because you can describe functions that accept any value without having any values in your function body.
+/*
+//Conversely, you can describe a function that returns a value of unknown type:
+function safeParse(s: string): unknown {
+  return JSON.parse(s);
+}
+// Need to be careful with 'obj'!
+const obj = safeParse(someRandomString);
+*/
+
+//note never
+//Some functions never return a value:
+function fail(msg: string): never {
+  throw new Error(msg);
+}
+
+//The never type represents values which are never observed.
+//In a return type, this means that the function throws an exception or terminates execution of the program.
+
+//never also appears when TypeScript determines there's nothing left in a union.
+//example
+function fn5(x: string | number) {
+  if (typeof x === "string") {
+    // do something
+  } else if (typeof x === "number") {
+    // do something else
+  } else {
+    x; // has type 'never'!
+  }
+}
+
