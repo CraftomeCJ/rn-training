6dda9c42fd32df7617b9a9e193433dc15cc667b7
javier ho <tech@craftome.com>
2022-04-09 23:30:27 +0800

    Learn Getters/Setters & Index Signature

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -238,3 +238,54 @@ class Method {
 const style = new Method();
 style.scale(20); //<== default values
 console.log(style.x, style.y);
+
+
+//Learn Getters/Setters
+//Classes can also have accessors:
+//example
+class GetterSetter {
+  _length = 0;
+  get length() {
+    return this._length;
+  }
+  set length(value) {
+    this._length = value;
+  }
+}
+//important Note that a field-backed get/set pair with no extra logic is very rarely useful in JavaScript. It's fine to expose public fields if you don't need to add additional logic during the get/set operations.
+
+//note TypeScript has some special inference rules for accessors:
+// =>If get exists but no set, the property is automatically readonly
+// =>If the type of the setter parameter is not specified, it is inferred from the return type of the getter
+// =>Getters and setters must have the same Member Visibility
+
+////Since TypeScript 4.3, it is possible to have accessors with different types for getting and setting.
+//example
+class Thing {
+  _size = 0;
+
+  get size(): number {
+    return this._size;
+  }
+  set size(value: string | number | boolean) {
+    let num = Number(value);
+
+    // Don't allow NaN, Infinity, etc
+    if (!Number.isFinite(num)) {
+      this._size = 0;
+      return;
+    }
+    this._size = num;
+  }
+}
+
+//note Index Signatures
+//Classes can declare index signatures; these work the same as Index Signatures for other object types:
+//example
+class MyClass {
+  [s: string]: boolean | ((s: string) => boolean);
+
+  check(s: string) {
+    return this[s] as boolean;
+  }
+}
