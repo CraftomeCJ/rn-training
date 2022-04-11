531110ec67935310a55ab085a5dbafc6b6dad22c
javier ho <tech@craftome.com>
2022-04-11 20:06:18 +0800

    TypeScript’s Namespaces

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -296,4 +296,8 @@ important Note that ES2020 is effectively the same as the original index.ts.
 You can see all of the available options and what their emitted JavaScript code looks like in the TSConfig Reference for module.
 */
 
-
+//learn TypeScript namespaces
+//TypeScript has its own module format called namespaces which pre-dates the ES Modules standard.
+//This syntax has a lot of useful features for creating complex definition files, and still sees active use in DefinitelyTyped.
+//While not deprecated, the majority of the features in namespaces exist in ES Modules and we recommend you use that to align with JavaScript's direction.
+//You can learn more about namespaces in the namespaces reference page.
