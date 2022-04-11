31656c389b765c8bdb929b5d7d9c23ad8d1b853e
javier ho <tech@craftome.com>
2022-04-11 13:22:30 +0800

    Interface as Object Type

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -15,4 +15,26 @@ const Info: React.FC = () =>
   );
 export default Info;
 
+
+
 //LEARN Typescript Basics: Object Types
+
+//note interface as Object Type
+//In JavaScript, the fundamental way that we group and pass around data is through objects. In TypeScript, we represent those through object types.
+//As we've seen, they can be anonymous:
+function greet(person: {
+  name: string;
+  age: number
+})
+{
+  return "Hello " + person.name;
+}
+
+////or they can be named by using either an interface as Object Type
+interface InterfacePerson {
+  name: string;
+  age: number;
+}
+const greetThere = (person: InterfacePerson) => {
+  return "Hello " + person.name;
+}
