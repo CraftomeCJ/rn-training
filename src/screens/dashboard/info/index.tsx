25ce59ebf063fb507dd813137bd8c4b6c5ebe22b
javier ho <tech@craftome.com>
2022-04-11 15:29:51 +0800

    Extending Type - Extend

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -277,3 +277,60 @@ myArray[2] = "Mallory";
 
 //note You can't set myArray[2] because the index signature is readonly.
 */
+
+
+//learn Extending Types
+//It's pretty common to have types that might be more specific versions of other types.
+//example, we might have a BasicAddress type that describes the fields necessary for sending letters and packages in the U.S.
+//note interface as Object Type with Optional Property
+interface BasicAddress {
+  name?: string;  //<== optional property
+  street: string; //<== object property
+  city: string;
+  country: string;
+  postalCode: string;
+}
+
+//In some situations that's enough, but addresses often have a unit number associated with them if the building at an address has multiple units. We can then describe an AddressWithUnit.
+interface AddressWithUnit {
+  name?: string;
+  unit: number;
+  street: string;
+  city: string;
+  country: string;
+  postalCode: string;
+}
+
+//This does the job, but the downside here is that we had to repeat all the other fields from BasicAddress when our changes were purely additive.
+//note Instead, we can extend the original BasicAddress type and just add the new fields that are unique to AddressWithUnit.
+interface BasicAddress {
+  name?: string;
+  street: string;
+  city: string;
+  country: string;
+  postalCode: string;
+}
+
+interface AddressWithUnit extends BasicAddress {
+  units: number;
+}
+
+//The extends keyword on an interface allows us to effectively copy members from other named types, and add whatever new members we want.
+//This can be useful for cutting down the amount of type declaration boilerplate we have to write, and for signaling intent that several different declarations of the same property might be related.
+//example, AddressWithUnit didn’t need to repeat the street property, and because street originates from BasicAddress, a reader will know that those two types are related in some way.
+
+//interfaces can also extend from multiple types.
+interface Colorful {
+  color: string;
+}
+
+interface Circle {
+  radius: number;
+}
+
+interface ColorfulCircle extends Colorful, Circle {}
+
+const cc: ColorfulCircle = {
+  color: "red",
+  radius: 42,
+};
